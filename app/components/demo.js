3aacc17290b8b9d4e734a7d1a27ea2d961e735e8
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-12-02 18:15:27 -0500

    helper and component work, modifier does not

--- app/components/demo.js
+++ app/components/demo.js
@@ -3,7 +3,7 @@ import { setComponentTemplate } from '@ember/component';
 import { hbs } from 'ember-cli-htmlbars';
 import templateOnly from '@ember/component/template-only';
 
-const MyComponent = setComponentTemplate(hbs`@first`, templateOnly());
+const MyComponent = setComponentTemplate(hbs`{{@first}}`, templateOnly());
 
 export default class DemoComponent extends Component {
   myComponent = MyComponent;
